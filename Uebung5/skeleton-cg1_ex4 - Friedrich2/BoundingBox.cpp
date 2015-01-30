09931e9013b77d422577fe83c283db5113ddeed2
Magnati <magnati@web.de>
2015-01-30 17:30:57 +0100

    BBox und KDBaum
    Fertig geschrieben. Frage ist ob es funktionert.

--- Uebung5/skeleton-cg1_ex4 - Friedrich2/BoundingBox.cpp
+++ Uebung5/skeleton-cg1_ex4 - Friedrich2/BoundingBox.cpp
@@ -1,10 +1,82 @@
-#include "BoundingBox.h"
+#pragma once
+
+#include "BoundingBox.hpp"
 #include "glm/glm/glm.hpp"
 
-BoundingBox::BoundingBox(void)
-{
+BoundingBox::BoundingBox(){
 }
 
-BoundingBox::BoundingBox(glm::uvec3 face){
+BoundingBox::~BoundingBox(){
+}
+
+BoundingBox::BoundingBox( glm::vec3 boundingBoxMin, glm::vec3 boundingBoxMax){
+	this->bBoxMax = boundingBoxMax;
+	this->bBoxMin = boundingBoxMin;
+	this->midPoint = glm::vec3((bBoxMax.x+bBoxMin.x)/2,(bBoxMax.y+bBoxMin.y)/2,(bBoxMax.z+bBoxMin.z)/2);
+}
+
+// 0 = x-axis, 1 = y-Axis, 2 = z-axis
+int BoundingBox::get_longest_axis(){
+	float x = abs(this->bBoxMax.x - this->bBoxMin.x);
+	float y = abs(this->bBoxMax.y - this->bBoxMin.y);
+	float z = abs(this->bBoxMax.z - this->bBoxMin.z);
+
+	if(x > y){
+		if(x>z){
+			return 0;
+		}else{
+			return 2;
+		}
+	}else{
+		if(y > z){
+			return 1;
+		}else{
+			return 2;
+		}
+	}
 
 }
+
+
+BoundingBox BoundingBox::get_bounding_box(std::vector<glm::uvec3> triangles, std::vector<glm::vec3> positions){
+  glm::vec3 boundingBoxMin= glm::vec3(std::numeric_limits<float>::max());
+  glm::vec3 boundingBoxMax= glm::vec3(std::numeric_limits<float>::min());
+  glm::uvec3 t;
+
+  for(unsigned int i= 0; i<triangles.size(); i++){
+	  t = triangles[i];
+	  glm::vec3 first,second,third;
+
+	  first = positions[t.x];
+	  if(first.x < boundingBoxMin.x) boundingBoxMin.x= first.x;
+	  if(first.x > boundingBoxMax.x) boundingBoxMax.x= first.x;
+	  if(first.y < boundingBoxMin.y) boundingBoxMin.y= first.y;
+	  if(first.y > boundingBoxMax.y) boundingBoxMax.y= first.y;
+	  if(first.z < boundingBoxMin.z) boundingBoxMin.z= first.z;
+	  if(first.z > boundingBoxMax.z) boundingBoxMax.z= first.z;
+
+	  second = positions[t.y];
+	  if(second.x < boundingBoxMin.x) boundingBoxMin.x= second.x;
+	  if(second.x > boundingBoxMax.x) boundingBoxMax.x= second.x;
+	  if(second.y < boundingBoxMin.y) boundingBoxMin.y= second.y;
+	  if(second.y > boundingBoxMax.y) boundingBoxMax.y= second.y;
+	  if(second.z < boundingBoxMin.z) boundingBoxMin.z= second.z;
+	  if(second.z > boundingBoxMax.z) boundingBoxMax.z= second.z;
+	 
+	  third = positions[t.z];
+	  if(third.x < boundingBoxMin.x) boundingBoxMin.x= third.x;
+	  if(third.x > boundingBoxMax.x) boundingBoxMax.x= third.x;
+	  if(third.y < boundingBoxMin.y) boundingBoxMin.y= third.y;
+	  if(third.y > boundingBoxMax.y) boundingBoxMax.y= third.y;
+	  if(third.z < boundingBoxMin.z) boundingBoxMin.z= third.z;
+	  if(third.z > boundingBoxMax.z) boundingBoxMax.z= third.z;	  
+		
+	  }
+	// So jetzt haben wir die extremen Ecken in dieser Gruppe Dreiecke.
+
+
+  return BoundingBox(boundingBoxMin, boundingBoxMax);
+  
+}
+
+
--- Uebung5/skeleton-cg1_ex4 - Friedrich2/KDNode.cpp
+++ Uebung5/skeleton-cg1_ex4 - Friedrich2/KDNode.cpp
@@ -1,12 +1,16 @@
-#include "KDNode.h"
+#include <vector>
+#include "KDNode.hpp"
+#include "BoundingBox.hpp"
 
 
-KDNode::KDNode(void)
+KDNode::KDNode()
+{
+}
+KDNode::~KDNode()
 {
 }
 
-
-KDNode* KDNode::build(std::vector<glm::uvec3*>& triangles, int depth) const{
+KDNode* KDNode::build(std::vector<glm::uvec3> triangles, std::vector<glm::vec3> positions, int depth) const{
 	KDNode* node = new KDNode();
 	node->faces = triangles;
 	node->left = NULL;
@@ -18,15 +22,73 @@ KDNode* KDNode::build(std::vector<glm::uvec3*>& triangles, int depth) const{
 		return node;
 	}
 
+	node->bbox = BoundingBox::get_bounding_box(triangles, positions);
+
 	//Sind wir in einem Blatt mit genau einem Triangle
-	if(faces.size() == 1){
-		node->bbox = triangles[0]->get_bounding_box();
+	if(faces.size() == 1){		
 		node->left = new KDNode();
 		node->right = new KDNode();
-		node->left->faces = std::vector<glm::uvec3*>();
-		node->right->faces = std::vector<glm::uvec3*>();
+		node->left->faces = std::vector<glm::uvec3>();
+		node->right->faces = std::vector<glm::uvec3>();
 		return node;
 	}
 
+	glm::vec3 midPoint = node->bbox.midPoint;
 
+	//Jetzt aufteilen
+
+	std::vector<glm::uvec3> leftTris;
+	std::vector<glm::uvec3> rightTris;
+	int axis = node->bbox.get_longest_axis();
+
+	for(int i = 0; i < triangles.size(); i++){
+		glm::vec3 currentMidPoint = get_midPoint_tr(triangles[i], positions);
+		switch (axis){
+		case 0: // x-Axis	
+			if(midPoint.x >= currentMidPoint.x){
+				rightTris.push_back(triangles[i]);
+			}else{
+				leftTris.push_back(triangles[i]);
+			}
+			break;
+		case 1: // y-axis
+			if(midPoint.y >= currentMidPoint.y){
+				rightTris.push_back(triangles[i]);
+			}else{
+				leftTris.push_back(triangles[i]);
+			}
+			break;
+		case 2: //z-Axis
+			if(midPoint.z >= currentMidPoint.z){
+				rightTris.push_back(triangles[i]);
+			}else{
+				leftTris.push_back(triangles[i]);
+			}
+			break;
+		}
+	}
+
+	// Haben die Dreiecke in beide Hälften aufgeteilt.
+	// Jetzt kommt noch die Rekursion
+
+	node->left = build(leftTris,positions,depth+1);	
+	node->right = build(rightTris,positions,depth+1);
+
+	return node;
+
+}
+
+
+glm::vec3 KDNode::get_midPoint_tr(glm::uvec3 tri, std::vector<glm::vec3> positions) const{
+	glm::vec3 result,first,second,third;
+
+	first = positions[tri.x];
+	second = positions[tri.y];
+	third = positions[tri.z];
+
+	result.x = (first.x+second.x+third.x)/3;
+	result.y = (first.y+second.y+third.y)/3;
+	result.z = (first.z+second.z+third.z)/3;
+
+	return result;
 }
